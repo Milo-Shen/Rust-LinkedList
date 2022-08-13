8976ddc0443dcf18a774c7b4fb3320069ce8705c
MiloShen <Milo-Shen@outlook.com>
2022-08-13 21:40:01 +0800

    feat: add implementation of linked node

--- src/first.rs
+++ src/first.rs
@@ -1,6 +1,5 @@
-// 所以我们应该怎么避免多余的 junk，保持内存分配的一致性，还能保持 null 指针优化呢？
-// 枚举可以让我们声明一个类型用于表达多个不同的值，而结构体可以声明一个类型同时包含多个值，只要将这两个类型结合在一起，
-// 就能实现之前的目标: 枚举类型用于表示 List，结构体类型用于表示 Node.
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -23,19 +22,29 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: std::mem::replace(&mut self.head, Link::Empty),
+            next: mem::replace(&mut self.head, Link::Empty),
         });
 
         self.head = Link::More(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match std::mem::replace(&mut self.head, Link::Empty) {
+        match mem::replace(&mut self.head, Link::Empty) {
             Link::Empty => None,
             Link::More(node) => {
                 self.head = node.next;
                 Some(node.elem)
             }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
         }
     }
 }
